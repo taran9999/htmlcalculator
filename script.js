8c7aac747ef0d658c972efcc043e43f4df6c08f5
taran9999 <taran184@gmail.com>
2022-06-14 13:51:56 -0700

    Wrote simple operate function for two numbers

---
+++ script.js
@@ -0,0 +1,15 @@
+function operate(op, a, b) {
+    switch(op) {
+        case "+":
+            return a + b;
+
+        case "-":
+            return a - b;
+
+        case "*":
+            return a * b;
+
+        case "/":
+            return a / b;
+    }
+}
