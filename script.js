6c09b9a45d1ffae12367a5554b167dea7008e5fc
taran9999 <taran184@gmail.com>
2022-06-20 12:42:19 -0700

    Created a basic outline for a graphical calculator

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const calcControls = document.getElementById('calc-controls');
+
+for(let i = 0; i < 4; i++) {
+    let r = document.createElement('div');
+    r.classList.add("control-rows");
+    for(let j = 0; j < 5; j++) {
+        let b = document.createElement('div');
+        b.classList.add("control-buttons");
+        r.appendChild(b);
+    }
+    calcControls.appendChild(r);
+}
+
 function operate(op, a, b) {
     switch(op) {
         case "+":
