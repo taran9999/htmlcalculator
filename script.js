a4193af712b68528e3cd9a182af303a4551a5224
taran9999 <taran184@gmail.com>
2022-07-12 12:12:13 -0700

    Created a function which correctly updates the number displayed, log statements for testing

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const calcControls = document.getElementById('calc-controls');
+const calcDisplay = document.getElementById('calc-display');
+let op1 = "";
+let op = "";
+let op2 = "";
+let displayUpdated = false;
+let updateDisplayRequest = false;
+
+calcDisplay.appendChild(document.createTextNode(""));
 
 for(let i = 0; i < 4; i++) {
     let r = document.createElement('div');
@@ -6,6 +14,9 @@ for(let i = 0; i < 4; i++) {
     for(let j = 0; j < 5; j++) {
         let b = document.createElement('div');
         b.classList.add("control-buttons");
+        b.onclick = () => {
+            if(b.textContent) updateDisplay(b.textContent);
+        }
         r.appendChild(b);
     }
     calcControls.appendChild(r);
@@ -16,6 +27,7 @@ const controlButtons = document.querySelectorAll(".control-buttons");
 controlButtons[15].appendChild(document.createTextNode("0"));
 controlButtons[16].appendChild(document.createTextNode("."));
 controlButtons[17].appendChild(document.createTextNode("="));
+controlButtons[18].appendChild(document.createTextNode("C"));
 controlButtons[10].appendChild(document.createTextNode("1"));
 controlButtons[11].appendChild(document.createTextNode("2"));
 controlButtons[12].appendChild(document.createTextNode("3"));
@@ -30,7 +42,9 @@ controlButtons[0].appendChild(document.createTextNode("7"));
 controlButtons[1].appendChild(document.createTextNode("8"));
 controlButtons[2].appendChild(document.createTextNode("9"));
 
-function operate(op, a, b) {
+function operate(op, x, y) {
+    let a = parseInt(x);
+    let b = parseInt(y);
     switch(op) {
         case "+":
             return a + b;
@@ -44,4 +58,64 @@ function operate(op, a, b) {
         case "/":
             return a / b;
     }
+}
+
+function updateDisplay(s) {
+    if(s == "C") {
+        calcDisplay.textContent = "";
+        op1 = "";
+        op = "";
+        op2 = "";
+    } else if(s == "+" || s == "-" || s == "*" || s == "/") {
+        if(calcDisplay.textContent != "") {
+            if(op1 == "") {
+                op1 = calcDisplay.textContent;
+                console.log(`op1 = ${calcDisplay.textContent}`);
+                op = s;
+                console.log(`op = ${s}`);
+                updateDisplayRequest = true;
+                console.log("updateDisplayRequest = true");
+                displayUpdated = false;
+                console.log("displayUpdated = false");
+            } else if(displayUpdated && op != "") {
+                op2 = calcDisplay.textContent;
+                console.log(`op2 = ${calcDisplay.textContent}`);
+                calcDisplay.textContent = "" + operate(op, op1, op2)
+                console.log(`calculation: ${op1} ${op} ${op2}`);
+                op1 = calcDisplay.textContent;
+                console.log(`op1 = ${calcDisplay.textContent}`);
+                op2 = "";
+                console.log("op2 = ");
+                updateDisplayRequest = true;
+                console.log("updateDisplayRequest = true");
+            }
+        }
+        op = s;
+        console.log(`op = ${s}`);
+    } else if(s == "=") {
+        if(op1 != "" && op != "" && displayUpdated) {
+            op2 = calcDisplay.textContent;
+            console.log(`op2 = ${calcDisplay.textContent}`);
+            calcDisplay.textContent = "" + operate(op, op1, op2)
+            console.log(`calculation: ${op1} ${op} ${op2}`);
+            op1 = calcDisplay.textContent;
+            console.log(`op1 = ${calcDisplay.textContent}`);
+            op2 = "";
+            console.log("op2 = ");
+            op = "";
+            console.log("op = ");
+            updateDisplayRequest = true;
+            console.log("updateDisplayRequest = true");
+        }
+    } else {
+        if(updateDisplayRequest) {
+            calcDisplay.textContent = "";
+            updateDisplayRequest = false;
+            console.log("updateDisplayRequest = false");
+            displayUpdated = true;
+            console.log("displayUpdated = true");
+        }
+        calcDisplay.textContent += s;
+    }
+    console.log(`op1: ${op1}, op: ${op}, op2: ${op2}, display: ${calcDisplay.textContent}`);
 }
